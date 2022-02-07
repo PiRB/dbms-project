4641b2467ad596cc3f27fcf61e3add053e0219a0
Pierre Brivio <pierrebrivio14@gmail.com>
2022-02-07 16:53:28 +0100

    add .env

--- app/utils/db.js
+++ app/utils/db.js
@@ -1,11 +1,12 @@
 const mariadb = require("mariadb");
+require('dotenv').config()
 
 const pool = mariadb.createPool({
-    host: '163.172.130.142',
-    port: '3310',
-    user: 'etudiant',
-    password: 'CrERP29qwMNvcbnAMgLzW9CwuTC5eJHn',
-    database: 'sakila'
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_SCHEMA
 });
 
 module.exports = pool;
