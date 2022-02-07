59fb6c0b5bab3e40a421f3f08c59246be56fb771
Pierre Brivio <pierrebrivio14@gmail.com>
2022-02-07 17:08:49 +0100

    add bulma to project

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,19 +4,6 @@ var pool = require('../utils/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const firstSelect = () => {
-    pool.getConnection()
-        .then(conn => {
-
-          conn.query("select actor_id, first_name, last_name from actor where last_name like 'B%'")
-              .then((rows) => {
-                console.log(rows);
-              })
-        }).catch(err => {
-      //not connected
-    });
-  }
-  console.log(firstSelect())
   res.render('index', { title: 'Express' });
 });
 
